2e92aa3ba6b06f6545737e6b3333c351345ec5da
Ken Sedgwick <ken@bonsai.com>
2025-03-19 16:52:44 -0700

    improve the readability and debugging of the jni init function

--- src/droidplug/jni/mod.rs
+++ src/droidplug/jni/mod.rs
@@ -2,57 +2,54 @@ pub mod objects;
 
 use ::jni::{objects::JObject, JNIEnv, JavaVM, NativeMethod};
 use jni::{objects::JString, sys::jboolean};
+use log::debug;
 use once_cell::sync::OnceCell;
 use std::ffi::c_void;
 
 static GLOBAL_JVM: OnceCell<JavaVM> = OnceCell::new();
 
 pub fn init(env: &JNIEnv) -> crate::Result<()> {
-    if let Ok(()) = GLOBAL_JVM.set(env.get_java_vm()?) {
-        env.register_native_methods(
-            "com/nonpolynomial/btleplug/android/impl/Adapter",
-            &[
-                NativeMethod {
-                    name: "reportScanResult".into(),
-                    sig: "(Landroid/bluetooth/le/ScanResult;)V".into(),
-                    fn_ptr: adapter_report_scan_result as *mut c_void,
-                },
-                NativeMethod {
-                    name: "onConnectionStateChanged".into(),
-                    sig: "(Ljava/lang/String;Z)V".into(),
-                    fn_ptr: adapter_on_connection_state_changed as *mut c_void,
-                },
-            ],
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/Peripheral",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/ScanFilter",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/NotConnectedException",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/PermissionDeniedException",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/UnexpectedCallbackException",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/UnexpectedCharacteristicException",
-        )?;
-        jni_utils::classcache::find_add_class(
-            env,
-            "com/nonpolynomial/btleplug/android/impl/NoSuchCharacteristicException",
-        )?;
+    debug!("JNI initialization starting");
+    match GLOBAL_JVM.set(env.get_java_vm()?) {
+        Ok(_) => debug!("Java VM successfully set in GLOBAL_JVM"),
+        Err(_) => debug!("Failed to set Java VM in GLOBAL_JVM"),
+    };
+
+    debug!("Registering native methods");
+    let class = env.auto_local(env.find_class("com/nonpolynomial/btleplug/android/impl/Adapter")?);
+    env.register_native_methods(
+        &class,
+        &[
+            NativeMethod {
+                name: "reportScanResult".into(),
+                sig: "(Landroid/bluetooth/le/ScanResult;)V".into(),
+                fn_ptr: adapter_report_scan_result as *mut c_void,
+            },
+            NativeMethod {
+                name: "onConnectionStateChanged".into(),
+                sig: "(Ljava/lang/String;Z)V".into(),
+                fn_ptr: adapter_on_connection_state_changed as *mut c_void,
+            },
+        ],
+    )?;
+    debug!("Native methods registered");
+
+    let classes = [
+        "com/nonpolynomial/btleplug/android/impl/Peripheral",
+        "com/nonpolynomial/btleplug/android/impl/ScanFilter",
+        "com/nonpolynomial/btleplug/android/impl/NotConnectedException",
+        "com/nonpolynomial/btleplug/android/impl/PermissionDeniedException",
+        "com/nonpolynomial/btleplug/android/impl/UnexpectedCallbackException",
+        "com/nonpolynomial/btleplug/android/impl/UnexpectedCharacteristicException",
+        "com/nonpolynomial/btleplug/android/impl/NoSuchCharacteristicException",
+    ];
+
+    for class in &classes {
+        debug!("Finding and adding class: {}", class);
+        jni_utils::classcache::find_add_class(env, class)?;
     }
+    debug!("All classes found and added successfully");
+
     Ok(())
 }
 
