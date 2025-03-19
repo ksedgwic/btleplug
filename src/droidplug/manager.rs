1f41cd8f403d16319249d747ecb0875f384f97fd
Ken Sedgwick <ken@bonsai.com>
2025-03-19 15:42:47 -0700

    enhanced debugging of startup

--- src/droidplug/manager.rs
+++ src/droidplug/manager.rs
@@ -1,12 +1,14 @@
 use super::adapter::Adapter;
 use crate::{api, Result};
 use async_trait::async_trait;
+use log::debug;
 
 #[derive(Clone, Debug)]
 pub struct Manager;
 
 impl Manager {
     pub async fn new() -> Result<Manager> {
+        debug!("droidplug::Manager::new");
         Ok(Manager)
     }
 }
@@ -16,6 +18,9 @@ impl api::Manager for Manager {
     type Adapter = Adapter;
 
     async fn adapters(&self) -> Result<Vec<Adapter>> {
-        Ok(vec![super::global_adapter().clone()])
+        debug!("droidplug::Manager::adapters starting");
+        let retval = vec![super::global_adapter().clone()];
+        debug!("droidplug::Manager::adapters finished");
+        Ok(retval)
     }
 }
--- src/droidplug/mod.rs
+++ src/droidplug/mod.rs
@@ -3,6 +3,7 @@ pub mod manager;
 pub mod peripheral;
 
 use ::jni::JNIEnv;
+use log::debug;
 use once_cell::sync::OnceCell;
 
 mod jni;
@@ -10,12 +11,21 @@ mod jni;
 static GLOBAL_ADAPTER: OnceCell<adapter::Adapter> = OnceCell::new();
 
 pub fn init(env: &JNIEnv) -> crate::Result<()> {
-    self::jni::init(env)?;
+    debug!("droidplug: jni::init starting");
+    match self::jni::init(env) {
+        Ok(_) => debug!("JNI initialization succeeded"),
+        Err(err) => {
+            debug!("JNI initialization failed: {:?}", err);
+            return Err(err);
+        }
+    }
+    debug!("droidplug: jni::init finished");
     GLOBAL_ADAPTER.get_or_try_init(|| adapter::Adapter::new())?;
     Ok(())
 }
 
 pub fn global_adapter() -> &'static adapter::Adapter {
+    debug!("droidplug::global_adapter starting");
     GLOBAL_ADAPTER.get().expect(
         "Droidplug has not been initialized. Please initialize it with btleplug::platform::init().",
     )
